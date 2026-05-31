faa1a6fd007ec12461a53ba0297cf2b632126646
INUOLAJI <belloinuolaji@gmail.com>
2026-05-31 22:50:26 +0100

    your message

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+vimport { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  'https://mtkecdfrvpbphdjciyht.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10a2VjZGZydnBicGhkamNpeWh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzNzY4MDAsImV4cCI6MjA5NDk1MjgwMH0.oQeiGQO8CFYs_Wh3Yr6FU23_vZkrRSV6fLtUL_yoh3k'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
 
 export default supabase;
